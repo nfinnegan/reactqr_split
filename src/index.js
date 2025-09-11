9eec2187f6fe7257fa4148ebcaa36a86effa350e
nfinnegan <natalie.finnegan@harness.io>
2025-09-11 15:02:13 -0500

    app spin up

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SplitFactoryProvider } from '@splitsoftware/splitio-react';
+
+const sdkConfig = {
+  core: {
+    authorizationKey: "1lp5a0rjsn1lidv5rvpcfhv0dineok7cbjhm",
+    key: "natalie_test"
+
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <SplitFactoryProvider config={sdkConfig}> 
     <App />
+  </SplitFactoryProvider>   
   </React.StrictMode>
 );
 
