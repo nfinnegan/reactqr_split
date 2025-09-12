d30abdcda18b15e23dab82d94a99368a059620c5
nfinnegan <natalie.finnegan@harness.io>
2025-09-12 16:17:21 -0500

    added second flag

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState,useEffect, use} from 'react';
 import { useSplitTreatments } from '@splitsoftware/splitio-react';
 import QRCode from "react-qr-code";
 //import qrCodeHome from './components/qrCode';
 import { osName, deviceType } from 'react-device-detect';
 
 function App() {
-
+  
+  //Change QRURL to the URL where you'll be hosting this app
   const QRURL = "https://nfinnegan.github.io/reactqr_split/";
-  const featureName = 'show_qrCode';
+  const showQR = 'show_qrCode';
+  const configBackground = 'configureBackground';
 
   const { treatments, isReady } = useSplitTreatments({
-    names: [featureName],
+    names: [showQR, configBackground],
     attributes:{
       device: deviceType,
       operatingSystem: osName
@@ -26,15 +28,20 @@ function App() {
     return <div>Loading...</div>;    
   }
 
-  const { treatment } = treatments[featureName] || {};
+  //access each treatmnet individually
+  const showQRTreatment = treatments[showQR]?.treatment
+  const configBackgroundTreatment = treatments[configBackground]?.treatment
+
+  console.log("showQRTreatment", showQRTreatment);
+  console.log("configBackgroundTreatment", configBackgroundTreatment);
 
   return (
-  <div>
+  <div className={`${configBackgroundTreatment}`}>
       <br />
       <span className="container-qr" style={{ color: 'black' }}>
         <h3>Scan me!</h3>
           <div className="qr-wrapper">
-            {treatment === 'on' ?  <QRCode value={QRURL} /> :  <div></div>}   
+            {showQRTreatment === 'on' ?  <QRCode value={QRURL} /> :  <div></div>}   
           </div>
       </span>
   </div>
